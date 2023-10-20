d9d28978b92222b1d7c236f0219748c4579c083c
huguesbert17 <huguesbert17@gmail.com>
2023-10-20 03:49:58 -0400

    Creating home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/App.scss';
+import { ChakraProvider } from '@chakra-ui/react'
+import Loader from "./components/Loader";
+import {BrowserRouter} from "react-router-dom";
+import theme from "./theme/theme";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <Loader/>
+    </BrowserRouter>
+  </ChakraProvider>
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,58 @@
+import {FC} from "react";
+import {NavLink, Link as ReactRouter} from "react-router-dom"
+import {
+    Box,
+    Flex,
+    HStack,
+    IconButton,
+    Popover,
+    PopoverTrigger,
+    useDisclosure,
+    Image,
+    Text,
+    Link,
+    Button, Stack, useTheme, PopoverContent, Container
+} from "@chakra-ui/react";
+import {HamburgerIcon, CloseIcon, ChevronDownIcon} from '@chakra-ui/icons';
+// import Logo from "../../../assets/img/logo.svg"
+
+
+
+interface IProps {
+
+}
+
+const NavBar: FC<IProps> = (props: IProps) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return <>
+        <Box bg={"white"} px={8} as="header" backgroundColor={"rgba(255, 255, 255, 0.4)"}backdropFilter={"saturate(180%) blur(20px)"} zIndex={1} position="fixed" w="100%"  >
+            <Flex h={20} alignItems={'center'} justifyContent={"space-between"} height={{md: "3rem"}}>
+                <IconButton size={'md'} icon={isOpen ? <CloseIcon /> : <HamburgerIcon />} aria-label={'Open Menu'} display={{ md: 'none' }} onClick={isOpen ? onClose : onOpen}/>
+                <Box>
+                    <NavLink to="/">
+                        {/*<Image width={150} src={Logo} alt='logo'/>*/}
+                    </NavLink>
+                </Box>
+                <HStack spacing={8}>
+                    <HStack spacing={6} color="brand.dark" display={{ base: 'none', md: 'flex' }} fontSize={"16px"} fontWeight={400}>
+                        <Link _hover={{color: "brand.gray", textDecoration: "none"}} as={NavLink} to="/flights">Flights</Link>
+                        <Link _hover={{color: "brand.gray", textDecoration: "none"}} as={NavLink} to="/stays">Stays</Link>
+                        <Link _hover={{color: "brand.gray", textDecoration: "none"}} as={NavLink} to="/cars">Cars</Link>
+                        <Link _hover={{color: "brand.gray", textDecoration: "none"}} as={NavLink} to="/Cruises">Cruises</Link>
+                        <Link _hover={{color: "brand.gray", textDecoration: "none"}} as={NavLink} to="/flight-tracker">Flight tracker</Link>
+                    </HStack>
+                </HStack>
+            </Flex>
+
+            {isOpen ? (
+                <Box pb={4} display={{ md: 'none' }}>
+                    <Stack as={'nav'} spacing={4}>
+                        <NavLink to="Home">Home</NavLink>
+                    </Stack>
+                </Box>
+            ) : null}
+        </Box>
+    </>
+
+}
+export default NavBar
